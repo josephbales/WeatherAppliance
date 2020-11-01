60ee599287167ed6ac290839ff39ac7eddee0dda
Joseph Bales <joseph.bales@gmail.com>
2020-10-31 19:10:08 -0500

    removing some icons I don't plan to use and working on the drawing facade to produce a sample that is more like what I want

--- Facades/DrawingFacade.py
+++ Facades/DrawingFacade.py
@@ -1,6 +1,12 @@
 import os
 import svgwrite
 
+STYLES = """.conditions { font-family: sans-serif; font-size: 18px; font-weight: bold; fill: black; }
+.message { font-family: sans-serif; font-size: 18px; font-weight: bold; fill: black; }
+.last-updated { font-family: sans-serif; font-size: 12px; font-weight: 900; fill: black; }
+.temperature { font-family: sans-serif; font-size: 80px; font-weight: bold; text-transform: uppercase; fill: black; }
+"""
+
 
 class DrawingFacade(object):
 
@@ -28,30 +34,60 @@ class DrawingFacade(object):
         desktop = os.path.join(os.path.join(home_dir), 'Desktop')
 
         main_image = os.path.join(desktop, 'test.svg')
-        bottom_image = os.path.join(desktop, 'bottom.svg')
-        left_image = os.path.join(desktop, 'left.svg')
-        right_image = os.path.join(desktop, 'right.svg')
+        # bottom_image = os.path.join(desktop, 'bottom.svg')
+        # left_image = os.path.join(desktop, 'left.svg')
+        # right_image = os.path.join(desktop, 'right.svg')
         print(main_image)
 
-        bottom_bar = svgwrite.Drawing(bottom_image, size=(640, 84), x=0, y=0)
-        bottom_bar.viewbox(minx=0, miny=0, width=640, height=84)
-        bottom_bar.add(bottom_bar.text('There are currently no weather alerts for your location.',
-                                       insert=('50%', '50%'),
-                                       stroke='none',
-                                       fill='black',
-                                       font_size='18px',
-                                       font_weight='bold',
-                                       font_family='sans-serif',
-                                       dominant_baseline='middle',
-                                       text_anchor='middle'))
-        bottom_bar.save()
+        # bottom_bar = svgwrite.Drawing(bottom_image, size=(640, 84), x=0, y=0)
+        # bottom_bar.viewbox(minx=0, miny=0, width=640, height=84)
+        # bottom_bar.add(bottom_bar.text('There are currently no weather alerts for your location.',
+        #                                insert=('50%', '50%'),
+        #                                stroke='none',
+        #                                fill='black',
+        #                                font_size='18px',
+        #                                font_weight='bold',
+        #                                font_family='sans-serif',
+        #                                dominant_baseline='middle',
+        #                                text_anchor='middle'))
+        # bottom_bar.save()
+        left_top_g = svgwrite.container.Group()
 
-        main_drawing = svgwrite.Drawing(main_image, size=(640, 384), profile="tiny")
+        bottom_g = svgwrite.container.Group()
+
+        left_top_svg = svgwrite.container.SVG(insert=(0, 0), size=(400, 300))
+        # ideally would rather read this SVG as XML and insert it here?
+        conditions_image = svgwrite.image.Image(href='C:\\Users\\josep\\source\\repos\\WeatherAppliance\\images\\drop_weather_cloud_rain_forecasticon.svg')
+        left_top_svg.add(conditions_image)
+        left_top_g.add(left_top_svg)
+
+        right_top_g = svgwrite.container.Group()
+        right_top_svg = svgwrite.container.SVG(insert=(400, 0), size=(240, 300))
+        # Note that x and y must be sent as lists here because in the W3C spec it's a list and
+        # since class is a reserved work, we have to add an underscore behind it
+        temperature_text = svgwrite.text.Text('108°', x=['50%'], y=['50%'], dominant_baseline='middle', text_anchor='middle', class_="temperature")
+        right_top_svg.add(temperature_text)
+        right_top_g.add(right_top_svg)
+
+        rt_top_g = svgwrite.container.Group()
+        rt_top_svg = svgwrite.container.SVG(insert=(0, 0), size=(240, 200))
+        rt_top_g.add(rt_top_svg)
+
+        rt_bottom_g = svgwrite.container.Group()
+        rt_bottom_svg = svgwrite.container.SVG(insert=(0, 180), size=(240, 100))
+        rt_bottom_g.add(rt_bottom_svg)
+
+        right_top_svg.add(rt_top_g)
+        right_top_svg.add(rt_bottom_g)
+
+        main_drawing = svgwrite.Drawing(main_image, size=(640, 384))
         main_drawing.viewbox(minx=0, miny=0, width=640, height=384)
         main_drawing.add(main_drawing.polyline(points=[(0, 0), (640, 0), (640, 384), (0, 384), (0, 0)], stroke='black',
                                                stroke_width=10, fill='none'))
         main_drawing.add(
             main_drawing.polyline(points=[(0, 300), (640, 300), (640, 0), (400, 0), (400, 300)], stroke='black',
                                   stroke_width=5, fill='none'))
-        main_drawing.add(main_drawing.image(bottom_image, insert=(0, 300)))
+        main_drawing.add(left_top_g)
+        main_drawing.add(right_top_g)
+        main_drawing.defs.add(main_drawing.style(STYLES))
         main_drawing.save()
--- Services/WeatherGovService.py
+++ Services/WeatherGovService.py
@@ -10,29 +10,23 @@ class WeatherGovService(object):
 
     def get_alerts_by_zone(self, zone):
         url = f"/alerts/active/zone/{zone}"
-        return self.__get_request__(url)
+        return self.__get_request__(self._base_url + url)
     
     def get_current_conditions_by_station(self, station):
         url = f"/stations/{station}/observations/latest"
-        return self.__get_request__(url)
+        return self.__get_request__(self._base_url + url)
 
     def get_station_info(self, station):
         url = f"/stations/{station}"
-        return self.__get_request__(url)
+        return self.__get_request__(self._base_url + url)
 
-    def get_icon_by_url(self, url):
-        response = requests.get(url, headers=self._headers)
-        return response
+    def get_by_full_url(self, url):
+        return self.__get_request__(url)
         
     def __get_request__(self, url, params=None):
-        # print(self._base_url + url)
-        response = requests.get(self._base_url + url, headers=self._headers, params=params)
-        if response.status_code != 200:
-            # Do something here if it returns a bad request of some kind, only really care if it's a success
-            # If not a success then need to try again later
-            # If it fails too many times, need to display an alert on the screen
-            # Probably need to just pass back pass or fail out of here and then have a screen controller or something
-            # that handles that
-            pass
+        response = requests.get(url, headers=self._headers, params=params)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
 
-        return response.json()
