31c50772b2cf4ea7c857e3f9b1a02e6015f6667f
Joseph Bales <joseph.bales@gmail.com>
2020-11-09 22:10:05 -0600

    downloading a weather image

--- weatherappliance.py
+++ weatherappliance.py
@@ -2,11 +2,27 @@ import os, time, yaml
 from lib.imagegen import DrawingFacade
 from lib.weatherapi import WeatherApi
 
-file = open('config.yaml', 'r')
-config = yaml.load(file, Loader=yaml.FullLoader)
+config_file = open('config.yaml', 'r')
+config = yaml.load(config_file, Loader=yaml.FullLoader)
 
-wa = WeatherApi(config['apiOpenWeather']['baseUrl'], os.environ.get(config['apiOpenWeather']['authEnvVariable']))
-print(wa.get_current_conditions_by_zip_and_country('37076', 'US'))
+wa_params = {
+    'base_api_url': config['weatherApi']['baseUrl'],
+    'icon_url': config['weatherApi']['iconUrl'],
+    'icon_suffix': config['weatherApi']['iconSuffix'],
+    'auth_key': os.environ.get(config['weatherApi']['authEnvVariable'])
+}
+
+cc_params = {
+    'zipcode': config['weatherApi']['zipCode'],
+    'country_code': config['weatherApi']['countryCode']
+}
+
+wa = WeatherApi(**wa_params)
+print(wa.get_current_conditions(**cc_params))
+icon = wa.get_weather_icon('10d')
+icon_file = open('/mnt/c/Users/josep/Desktop/icon.png', 'wb')
+icon_file.write(icon)
+icon_file.close()
 
 df = DrawingFacade(config)
 df.draw_sample_svg()
